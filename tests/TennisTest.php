bc129e06299bbf808fb4ff7943d70f2a5274ebbe
gemapatricia <gp.herrera@usp.ceu.es>
2022-12-12 10:45:44 +0100

    Test 1

---
+++ tests/TennisTest.php
@@ -0,0 +1,16 @@
+<?php declare( strict_types = 1 );
+
+use App\Tennis;
+use PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
+
+Class TennisTest extends TestCase{
+
+    public function testInicio():void{
+        $objeto = new Tennis();
+        assertEquals("Love", $objeto->getMarcador1(), "No coincide la puntuacion inicial");
+    } 
+}
+
+?>
