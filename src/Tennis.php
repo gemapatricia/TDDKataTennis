60fd1ebe9686d81bf3fb71d0aed4082165f67f51
gemapatricia <gp.herrera@usp.ceu.es>
2022-12-12 10:46:36 +0100

    Test 1 ok

---
+++ src/Tennis.php
@@ -0,0 +1,14 @@
+<?php declare( strict_types = 1 );
+
+namespace App;
+
+Class Tennis{
+
+    private $marcador1="Love";
+
+    public function getMarcador1(){
+        return $this->marcador1;
+    }
+}
+
+?>
