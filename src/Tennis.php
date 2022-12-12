14cca57d24425d15238ae0646aa9c97ec39640b6
gemapatricia <gp.herrera@usp.ceu.es>
2022-12-12 10:49:06 +0100

    Test 2 ok

--- src/Tennis.php
+++ src/Tennis.php
@@ -5,9 +5,14 @@ namespace App;
 Class Tennis{
 
     private $marcador1="Love";
+    private $marcador2="Love";
 
     public function getMarcador1(){
         return $this->marcador1;
+    }
+
+    public function getMarcador2(){
+        return $this->marcador2;
     }
 }
 
